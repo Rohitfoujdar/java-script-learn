f4fa700f820fee6bfea6d490722e8867220671e4
Rohitfoujdar <rohitfoujdar8696@gmail.com>
2024-09-20 18:34:46 +0530

    learn number mathod and string mathod

---
+++ src/Numbermathod.js
@@ -0,0 +1,79 @@
+// Number mathod in java script
+
+// .Number()
+
+// .parselnt()
+
+// .parseFloat()
+
+// .isFinite()
+
+// .isInteger()
+
+// .toFixed(x)
+
+// .toPrecision(x)
+
+
+
+// .Number
+
+// let a= "20";
+
+// let b= Number(a)
+
+// document.write(b + 20);
+
+
+//  parseInt
+
+// let a= "20.00890";
+
+// let b= parseInt(a)
+
+// document.write(b);
+
+
+//parseFloat
+
+// let a= "20.00890";
+
+// let b= parseFloat(a);
+
+// document.write(b);
+
+
+//isFinite
+
+// let a= "20.00890";
+
+// let b= Number.isFinite(a);
+
+// document.write(b);
+
+
+// isInteger
+
+// let a= "20.00890";
+
+// let b= Number.isInteger(a);
+
+// document.write(b);
+
+
+// toFixed
+
+// let a= 20.00890;
+
+// let b= a.toFixed(2);
+
+// document.write(b);
+
+
+// toprecision
+
+let a= 20.29890;
+
+let b= a.toPrecision(4);
+
+document.write(b);
--- src/stringmathod.js
+++ src/stringmathod.js
@@ -125,10 +125,116 @@
 
 // trim
 
-let a= "                  java script      "
+// let a= "                  java script      "
 
 
-let b= a.trim();
-alert(b);
+// let b= a.trim();
+// alert(b);
 
 // document.write(b);
+
+
+
+// charAt
+
+// let a= "java script is now is update"
+
+// let b= a.charAt(5);
+
+// document.write(b);
+
+
+
+// charCodeAt
+
+// let a= "java script is now is update"
+
+// let b= a.charCodeAt(5);
+
+// document.write(b);
+
+
+
+// fromcharCode
+
+// let a= "java script is now is update"
+
+// let b= String.fromCharCode(65);
+
+// document.write(b);
+
+
+// concat
+
+// let a= "java script is a good language" + "  ";
+
+// let b= "and" + " ";
+
+// let c= "include with CSS" ;
+
+// let d = a.concat(b,c);
+
+// document.write(d);
+
+
+
+// split
+
+// let a= "java script is a good language";
+
+// let b= a.split("i");
+
+// document.write(b);
+
+
+
+// repeat
+
+// let a= "java script is a good language" + " ";
+
+// let b= a.repeat(2);
+
+// document.write(b);
+
+
+// slice
+
+// let a= "java script is a good language";
+
+// let b= a.slice(2, 11);
+
+// document.write(b);
+
+
+
+
+// substr
+
+// let a= "java script is a good language" + " ";
+
+// let b= a.substr(0, 11);
+
+// document.write(b);
+
+
+//substring
+
+// let a= "java script is a good language" + " ";
+
+// let b= a.substring(0, 12);
+
+// document.write(b);
+
+
+
+
+// toString
+
+let a= 20;
+
+let b= a.toString();
+
+document.write(b + 30);
+
+
+
